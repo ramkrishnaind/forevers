6be144462edc1d16b259dfb3775877f0b25b2dd5
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2022-09-11 19:03:51 +0530

    one more code added

--- pages/_app.js
+++ pages/_app.js
@@ -36,6 +36,14 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter();
   debugger;
   useEffect(() => {
+    var ads = document.getElementsByClassName("adsbygoogle").length;
+    for (var i = 0; i < ads; i++) {
+      try {
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      } catch (e) {}
+    }
+  }, []);
+  useEffect(() => {
     debugger;
     const { googletag } = window;
     if (googletag) {
