dd1d3168c9ae93703818a84fc63815e9aa4eef57
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2022-09-12 16:55:35 +0530

    Added more ads

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,29 +1,37 @@
 import React from "react";
 import styles from "./style.module.scss";
 import Link from "next/link";
+import Ad from "../Ad/Ad";
 function Footer() {
   return (
-    <div className={`${styles.container}`}>
-      <div className={`${styles.links} md:flex-row`}>
-        <Link href="/">
-          <a className="py-3 md:py-0 text-[.8rem] md:text-base">About Us</a>
-        </Link>
-        <Link href="/">
-          <a className="py-3 md:py-0 text-[.8rem] md:text-base">
-            Privacy Policy
-          </a>
-        </Link>
-        <Link href="/">
-          <a className="py-3 md:py-0 text-[.8rem] md:text-base">Disclaimer</a>
-        </Link>
-        <Link href="/">
-          <a className="py-3 md:py-0 text-[.8rem] md:text-base">Contact Us</a>
-        </Link>
+    <>
+      <Ad
+        currentPath={`above footer sections`}
+        dataAdSlot={"9786571390"}
+        className="hidden md:hidden"
+      />
+      <div className={`${styles.container}`}>
+        <div className={`${styles.links} md:flex-row`}>
+          <Link href="/">
+            <a className="py-3 md:py-0 text-[.8rem] md:text-base">About Us</a>
+          </Link>
+          <Link href="/">
+            <a className="py-3 md:py-0 text-[.8rem] md:text-base">
+              Privacy Policy
+            </a>
+          </Link>
+          <Link href="/">
+            <a className="py-3 md:py-0 text-[.8rem] md:text-base">Disclaimer</a>
+          </Link>
+          <Link href="/">
+            <a className="py-3 md:py-0 text-[.8rem] md:text-base">Contact Us</a>
+          </Link>
+        </div>
+        <p className="text-[.8rem] md:text-base">
+          Copyright &copy; 2022 | FOREVERS.IN | All Rights Are Reserved
+        </p>
       </div>
-      <p className="text-[.8rem] md:text-base">
-        Copyright &copy; 2022 | FOREVERS.IN | All Rights Are Reserved
-      </p>
-    </div>
+    </>
   );
 }
 
