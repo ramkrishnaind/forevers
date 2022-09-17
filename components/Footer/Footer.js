6ca39fb8e6ea8f9cc5118af2f2a9732d2d4c9759
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2022-09-17 18:26:51 +0530

    slug changed for footer ad

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,15 +2,31 @@ import React from "react";
 import styles from "./style.module.scss";
 import Link from "next/link";
 import Ad from "../Ad/Ad";
+import { useRouter } from "next/router";
 function Footer() {
+  const [slugChanged, setSlugChanged] = React.useState(false);
+  const router = useRouter();
+  const { id } = router.query;
+  React.useEffect(() => {
+    debugger;
+    !window.adsbygoogle
+      ? (window.adsbygoogle = window.adsbygoogle || []).push({})
+      : console.log("Adsbygoogle already exists");
+    setSlugChanged(true);
+    setTimeout(() => {
+      setSlugChanged(false);
+    }, 10);
+  }, [id]);
   return (
     <>
       <div className={`${styles.container}`}>
-        <Ad
-          currentPath={`above footer sections`}
-          dataAdSlot={"9786571390"}
-          // className="hidden md:hidden"
-        />
+        {!slugChanged && (
+          <Ad
+            currentPath={`above footer sections`}
+            dataAdSlot={"9786571390"}
+            // className="hidden md:hidden"
+          />
+        )}
         <div className={`${styles.links} md:flex-row`}>
           <Link href="/">
             <a className="py-3 md:py-0 text-[.8rem] md:text-base">About Us</a>
