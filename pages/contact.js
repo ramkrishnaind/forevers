2645d4c4e9d38d4c3573d75cdb559695300447c5
mrkpay <111171488+mrkpay@users.noreply.github.com>
2022-09-24 13:17:42 +0530

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,5 @@
 
 function Contact (){
-  return <div><h1>Contact Us on</h1><p>support-forevers@gmail.com</p></div>;
+  return <div><h1>Contact Us on</h1><p>forevers.sup@gmail.com</p></div>;
 }
 export default Contact
