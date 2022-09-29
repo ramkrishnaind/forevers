36e86becd264538ef0d9b578381d42f31f0063d5
mrkpay <111171488+mrkpay@users.noreply.github.com>
2022-09-29 14:12:48 +0530

    Update GenerateCoinBtn.js
    {/* Ads removed anas test

--- components/GenerateCoinBtn/GenerateCoinBtn.js
+++ components/GenerateCoinBtn/GenerateCoinBtn.js
@@ -94,16 +94,16 @@ function GenerateCoinBtn() {
       <button className={styles.btn} onClick={handleClick}>
          Generate Coin 
       </button>
-      <Ad currentPath="Generate Coin" dataAdSlot="2096306726" />
+     {/* Ads removed anas test <Ad currentPath="Generate Coin" dataAdSlot="2096306726" /> */}
     </>
   ) : (
     <>
       <MathematicalRecaptcha passCaptcha={setNotARobot} />
-      <Ad
+      {/* Ads removed anas test<Ad
         currentPath={`below captcha code`}
         dataAdSlot={"2219583315"}
         // className="hidden md:hidden"
-      />
+      />*/}
     </>
   );
 }
