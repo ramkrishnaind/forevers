7aec2aa5f0d2a399b752f5bcba586c8f98816218
mrkpay <111171488+mrkpay@users.noreply.github.com>
2022-09-29 11:10:53 +0530

    Update index.js
    ads commented left side bar

--- components/News/LeftAdvertisement/index.js
+++ components/News/LeftAdvertisement/index.js
@@ -5,9 +5,9 @@ import Ad from "../../Ad/Ad";
 const index = () => {
   return (
     <div className=" bg-[#F2F2F0] flex flex-col gap-3">
-      <Ad currentPath="Above TopNCategories" dataAdSlot="4977620935" />
+      {/* comment by anas left sidebar <Ad currentPath="Above TopNCategories" dataAdSlot="4977620935" />*/}
       <TopNCategories />
-      <Ad currentPath="Below TopNCategories" dataAdSlot="6023071307" />
+      {/* comment by anas left sidebar 2<Ad currentPath="Below TopNCategories" dataAdSlot="6023071307" />*/}
       <RandomNPosts />
     </div>
   );
