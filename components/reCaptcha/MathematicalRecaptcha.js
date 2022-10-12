23db3b669e3ea00102567af03005396b17876d8b
mrkpay <111171488+mrkpay@users.noreply.github.com>
2022-10-12 22:47:54 +0530

    Create MathematicalRecaptcha.js
    try2

--- components/reCaptcha/MathematicalRecaptcha.js
+++ components/reCaptcha/MathematicalRecaptcha.js
@@ -44,11 +44,13 @@ function MathematicalRecaptcha({ passCaptcha }) {
         onChange={(e) => setText(e.target.value)}
         className="text-center border-gray-300 rounded-lg px-3 py-2 my-3 border-2 "
       />
+      <p>text here
+          <Ad currentPath="Generate Coin" dataAdSlot="2792852820" /> 
+      </p>
       <button className={styles.btn} onClick={handleClick}>
         Submit
       </button>
     </div>
-    <Ad currentPath="captcha btn" dataAdSlot="2792852820" /> 
   );
 }
 
