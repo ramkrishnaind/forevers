dec6b9d0791fbe4219a71b8f9d41b8077c1e8687
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2022-10-24 15:13:18 +0530

    Both have been added

--- pages/_document.js
+++ pages/_document.js
@@ -4,14 +4,14 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        {/* <Script
+        <Script
           id="Adsense-id"
           data-ad-client="ca-pub-7006648733841921"
           async="true"
           strategy="beforeInteractive"
           crossorigin="anonymous"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        /> */}
+        />
         <script
           async
           crossorigin="anonymous"
