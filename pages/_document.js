537b25ffac3111697528e1c02da52e6fc8df19c6
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2022-10-24 15:10:55 +0530

    Script tag removed

--- pages/_document.js
+++ pages/_document.js
@@ -4,19 +4,19 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <Script
+        {/* <Script
           id="Adsense-id"
           data-ad-client="ca-pub-7006648733841921"
           async="true"
           strategy="beforeInteractive"
           crossorigin="anonymous"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        />
-        {/* <script
+        /> */}
+        <script
           async
           crossorigin="anonymous"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7006648733841921"
-        ></script> */}
+        ></script>
       </Head>
       <body>
         <Main />
