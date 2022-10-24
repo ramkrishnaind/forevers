6c04588c7ac1d0f610854463bd79eaa0a18669eb
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2022-10-24 14:54:07 +0530

    Added _document.js

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <Script
+          id="Adsense-id"
+          data-ad-client="ca-pub-7006648733841921"
+          async="true"
+          strategy="beforeInteractive"
+          crossorigin="anonymous"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        />
+        <script
+          async
+          crossorigin="anonymous"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7006648733841921"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
