5b3807b09e6a052f2e0d2b0b33dddf8ebb7b20fb
mrkpay <111171488+mrkpay@users.noreply.github.com>
2022-11-12 10:35:51 +0530

    Update _document.js
    test 3

--- pages/_document.js
+++ pages/_document.js
@@ -80,8 +80,8 @@ strategy="afterInteractive"
 (function(s,u,z,p){
 s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);
 }
-)(document.createElement('script'),'https://inklinkor.com/tag.min.js',5517255,document.body||document.documentElement) 
-}}
+)(document.createElement('script'),'https://inklinkor.com/tag.min.js',5517255,document.body||document.documentElement)`,
+  }}
 />
         <meta name="propeller" content="fbacef6fa9bcd14b32d00e977aa69329" />
       </Head>
