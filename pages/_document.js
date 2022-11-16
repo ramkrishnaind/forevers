6672ba16516cbc1b76b874c2742b51ec65e37c9e
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2022-11-16 14:31:42 +0530

    removed */}

--- pages/_document.js
+++ pages/_document.js
@@ -90,7 +90,7 @@ export default function Document() {
               )('offfurreton.com',5516260, document.createElement('script'))`,
           }}
         />
-        */}
+
         {/* multi tag ad <Script 
 id="https://inklinkor.com/tag.min.js"
 strategy="afterInteractive"
